7e57bb77a34d61360939fccb8ee38e47142de483
djkazunoko <djkazunoko@gmail.com>
2022-12-08 11:07:05 +0900

    標準入力をJSONファイルに保存

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,15 +1,43 @@
 #!/usr/bin/env node
 
-process.stdin.setEncoding("utf8");
+const fs = require("node:fs/promises");
+const readline = require("node:readline");
 
+async function exists(filePath) {
+  try {
+    await fs.access(filePath);
+    return true
+  } catch {
+    return false;
+  }
+}
+
+async function fileWriter() {
+  try {
+    const filePath = "./memos.json";
+    if (!(await exists(filePath))) {
+      await fs.writeFile(filePath, '[]');
+    }
+    const file = await fs.readFile(filePath, { encoding: "utf8" });
+    const memos = JSON.parse(file);
+    memos.push(lines)
+    const data = JSON.stringify(memos);
+    await fs.writeFile(filePath, data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+process.stdin.setEncoding("utf8");
 const lines = [];
-const reader = require("readline").createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
 });
-reader.on("line", (line) => {
+
+rl.on("line", (line) => {
   lines.push(line);
 });
-reader.on("close", () => {
-  console.log(lines);
+
+rl.on("close", () => {
+  fileWriter();
 });
