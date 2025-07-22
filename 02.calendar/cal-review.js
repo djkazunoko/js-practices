722f5d92236ddcc072cb507b950d56cca63874a6
djkazunoko <djkazunoko@gmail.com>
2025-07-22 14:16:46 +0900

    土曜日で改行

--- 02.calendar/cal-review.js
+++ 02.calendar/cal-review.js
@@ -7,10 +7,14 @@ const today = new Date();
 let month = argv.m || today.getMonth() + 1;
 let year = argv.y || today.getUTCFullYear();
 let last_date = new Date(year, month, 0).getDate();
+let first_day = new Date(year, month - 1, 1).getDay();
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
 for (let date = 1; date <= last_date; date++) {
   process.stdout.write(`${date}`);
+  if ((date + first_day) % 7 === 0) {
+    console.log();
+  }
 }
