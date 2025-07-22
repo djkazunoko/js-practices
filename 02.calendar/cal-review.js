77bd50668477e4061f77ee705c5a0de35b4689c9
djkazunoko <djkazunoko@gmail.com>
2025-07-22 13:14:04 +0900

    指定された月の全ての日付を表示

--- 02.calendar/cal-review.js
+++ 02.calendar/cal-review.js
@@ -6,6 +6,11 @@ const argv = minimist(process.argv.slice(2));
 const today = new Date();
 let month = argv.m || today.getMonth() + 1;
 let year = argv.y || today.getUTCFullYear();
+let last_date = new Date(year, month, 0).getDate();
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
+
+for (let date = 1; date <= last_date; date++) {
+  process.stdout.write(`${date}`);
+}
