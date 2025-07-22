32780add94be54787160758dcd80c3b1827f4b09
djkazunoko <djkazunoko@gmail.com>
2025-07-22 14:56:13 +0900

    リファクタリング
    letをconstに変更
    
    変数名をキャメルケースに変更
    
    日付の後ろの空白または改行の出力を修正

--- 02.calendar/cal-review.js
+++ 02.calendar/cal-review.js
@@ -4,18 +4,20 @@ import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
-let month = argv.m || today.getMonth() + 1;
-let year = argv.y || today.getUTCFullYear();
-let last_date = new Date(year, month, 0).getDate();
-let first_day = new Date(year, month - 1, 1).getDay();
+const month = argv.m || today.getMonth() + 1;
+const year = argv.y || today.getUTCFullYear();
+const lastDate = new Date(year, month, 0).getDate();
+const firstDate = new Date(year, month - 1, 1).getDay();
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("   ".repeat(first_day));
+process.stdout.write("   ".repeat(firstDate));
 
-for (let date = 1; date <= last_date; date++) {
-  process.stdout.write(`${date.toString().padStart(2)} `);
-  if ((date + first_day) % 7 === 0) {
+for (let date = 1; date <= lastDate; date++) {
+  process.stdout.write(date.toString().padStart(2));
+  if ((date + firstDate) % 7 === 0) {
     console.log();
+  } else {
+    process.stdout.write(' ');
   }
 }
